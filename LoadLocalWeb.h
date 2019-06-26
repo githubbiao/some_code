412c95ad7b16bb721dc521e9d2c5c80344c24258
黄启标 <qibiao@life.com>
2019-06-26 17:31:30 +0800

    加载本地网页 String

---
+++ LoadLocalWeb.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DTDemo
+//
+//  Created by WangDa Mac on 2019/6/24.
+//  Copyright © 2019 黄启标. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoadLocalWeb.m
@@ -0,0 +1,101 @@
+
+#define kHTML @"《蜘蛛侠：英雄远征》官宣定档6月28日，掐指一算，比北美还要早4天！<br>我们又是全球最早一起看漫威电影的观众诶~<br>想到世界各地的观众都要比咱们多等几天，Chris我对这个安排还是十分满意的。<br><img src=\"http://static.migucloud.com/1919/8b/62/23/aa/8e/834c6c6d72c99c99750f6d.jpg\" width=\"276\" height=\"241\" /img><br>临近电影上映，主演们开始马不停蹄地跑宣传。<br><br>Chris想问问大家，蜘蛛侠舔手掌心是什么感觉？<br>别人不知道，反正老吉是深有体会！<br><br>杰克·杰伦哈尔和荷兰弟在墨西哥宣传新电影《蜘蛛侠·英雄远征》的时候，<br>荷兰弟在剧透边缘的时候被老吉捂住了嘴，然后荷兰弟绝地反击直接舔了老吉的手心！<br><br>主持人：小荷兰快说说你们的新电影。<br>荷兰弟：我们... ...<br>老吉：别！！！<br>（捂住荷兰弟的嘴）<br>（噗呲，我吸！！！）<br>老吉：？？？ W!!!!!！ WTF！！！<br><img src=\"http://static.migucloud.com/1919/76/71/8f/03/01/2183b99e4e72fdbadc69ca.jpg\" width=\"412\" height=\"296\" /img><br><img src=\"http://static.migucloud.com/1919/1d/d3/d0/3b/02/8a9f523eb7704627da9201.jpg\" width=\"556\" height=\"513\" /img> 自5月28日起，荷兰弟现身巴厘岛，《蜘蛛侠：英雄远征》就开启了亚洲宣传。<br><img src=\"http://static.migucloud.com/1919/17/d6/3c/7f/d6/dfe51396adfb4131eb24d0.jpg\" width=\"612\" height=\"408\" /img><br>之后又到了韩国。<br>当然，很快，荷兰弟也将会来中国与我们见面了！<br><img src=\"http://static.migucloud.com/1919/58/55/f9/43/70/622a6392a7c8588bb25371.jpg\" width=\"556\" height=\"829\" /img><br>在巴厘岛接受采访时，一位中国博主教荷兰弟用四种方言说电影名称。<br>蜘煮虾：英雄眼睁？还是那个阔爱的大男孩罢了。<br>荷兰弟的中国之行中会发生什么趣事呢？<br>搓手期待。<br><br>一直都是邻家少年荷兰豆，如今秀上了在线健身，话说这超级英雄可不是说当就当的。<br>先秀一波单杠：<br><img src=\"http://static.migucloud.com/1919/5b/1c/47/b9/45/f6a3128a3fb101623bb34b.gif\" width=\"268\" height=\"151\" /img><br>再秀一波倒立：<br><img src=\"http://static.migucloud.com/1919/84/06/85/52/65/9b6dfbfdcbb399e6022715.gif\" width=\"268\" height=\"151\" /img><br>比你帅的人还比你努力，那你努力还有个屁用！（嗝...）<br>还记得复联三里妮妮抱着马上要化成灰的小蜘蛛吗？<br>原本一个让妮妮和漫威粉都心碎的场景，结果在荷兰弟的采访里变了味道：<br><img src=\"http://static.migucloud.com/1919/51/ca/bd/83/18/bd61a01ce14b3860eb5f01.jpg\" width=\"436\" height=\"580\" /img><br>行吧，你帅你有理。<br><img src=\"http://static.migucloud.com/1919/94/eb/3b/39/2c/81018cf01fb8e4d9ffc40c.jpg\" width=\"440\" height=\"300\" /img><br>当然，除了皮，荷兰弟和Ned还有让人感动的兄弟情。<br>主持人问：如果灭霸打完响指梅姨和Ned只能活一个怎么办？<br>荷兰弟：我选Ned！<br>主持人：美队和Ned呢？<br>荷兰弟：Ned！<br>主此人还不罢休：妮妮和Ned呢？<br>荷兰弟：Ned！Ned！！Ned！！！<br><img src=\"http://static.migucloud.com/1919/86/e8/7e/02/0b/30a2af3ba7073b717ca1b2.jpg\" width=\"360\" height=\"821\" /img><br>我酸了！<br><img src=\"http://static.migucloud.com/1919/17/a8/17/31/99/1dfbcbeb2039921cc4bd96.gif\" width=\"268\" height=\"330\" /img><br>妮妮离开的第N天，想他。<br>尽管如此，妮妮在影片中仍然存在感满满。<br>在预告片中，妮妮的名字就被提及数次，他的身影也悄悄地在电影中出现。<br><img src=\"http://static.migucloud.com/1919/2a/e3/9e/d0/75/a0329a557d15526634ce83.jpg\" width=\"556\" height=\"353\" /img><br>细心的粉丝发现，大家祭奠钢铁侠的墙上涂鸦都极为用心！<br>将下面这幅涂鸦进行反向处理，涂鸦会浮现出头盔内，托尼·史塔克的面孔，真的超级戳！<br><img src=\"http://static.migucloud.com/1919/44/ba/c2/61/21/a7f44dcc5d4c50634ce42b.jpg\" width=\"1080\" height=\"459\" /img><br><img src=\"http://static.migucloud.com/1919/1a/93/92/da/35/00a186ce92a770b602f0eb.jpg\" width=\"676\" height=\"676\" /img><br>这一次，蜘蛛侠没有了钢爸爸的照顾，终于要独立面对接下来的艰难险阻，成为一名真正的蜘蛛侠。<br><br>据悉，本片将会揭露钢铁侠的一个小秘密。<br>在《复联4》中，钢铁侠在穿越量子领域之前，就已经给复联众人录好了遗言。<br><img src=\"http://static.migucloud.com/1919/ee/3f/47/bc/39/68a7833838814a8f4bdbcc.jpg\" width=\"912\" height=\"480\" /img><br>与此同时，他很可能给小蜘蛛单独地留下了一些东西。<br>托尼有一个隐藏在史塔克工业的秘密实验室，里面存放着各种先进的科技和装备，这都是给小蜘蛛的宝贵财富。<br>在预告中，当我看到彼得带着钢铁侠的那副高科技眼镜，差点泪奔！<br><img src=\"http://static.migucloud.com/1919/c6/76/99/e0/a1/995468de085bd20a90e249.jpg\" width=\"556\" height=\"301\" /img><br>这不正是在《复联3》中，钢铁侠戴的那幅眼镜吗？<br><img src=\"http://static.migucloud.com/1919/e7/bb/06/d6/cb/1b0f5fc9710d250341a2eb.jpg\" width=\"1080\" height=\"740\" /img><br>他把一切都留给了彼得……<br><img src=\"http://static.migucloud.com/1919/cb/00/2f/82/1a/0bb503fb4b629a5ada9369.jpg\" width=\"556\" height=\"230\" /img><br>目测小虫这一次会点满科技属性，还原漫画，成为科武双修的英雄!<br>在最新预告里，我们看到，原来妮妮的那副眼镜还有获取通讯信息的功能。<br>哈哈哈，突然感觉妮妮太调皮了！<br>没眼看，没眼看！<br><img src=\"http://static.migucloud.com/1919/fd/4e/f4/dc/77/15f52bc4ace348ed8a43b2.gif\" width=\"480\" height=\"269\" /img><br>据说漫画版的钢铁蜘蛛战衣可能会在正片中出现，但是蜘蛛侠会不会穿上那就是另外一回事了。<br>此外，神秘客的真实身份到底是什么？这个故事又与“多元宇宙”有怎样的联系？<br>预告片可真是吊足了我们的胃口，这一切都需要我们去影院寻找答案。<br>所以，我们的口号是——<br>6月28 日，《蜘蛛侠：英雄远征》，约起来！"
+
+
+/**
+ 
+ void swizzle(Class originalClass, SEL originalSelector, Class swizzledClass, SEL swizzledSelector) {
+ 
+ Method originalMethod = class_getInstanceMethod(originalClass, originalSelector);
+ Method swizzledMethod = class_getInstanceMethod(swizzledClass, swizzledSelector);
+ 
+ BOOL didAddMethodInit = class_addMethod(originalClass, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+ 
+ if (didAddMethodInit) {
+ class_addMethod(originalClass, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+ }else{
+ method_exchangeImplementations(originalMethod, swizzledMethod);
+ }
+ }
+ 
+ 
+ **/
+
+
+#import "ViewController.h"
+#import <WebKit/WebKit.h>
+
+@interface ViewController ()<WKUIDelegate,WKNavigationDelegate>
+@property (nonatomic, strong) WKWebView *webview;
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    _webview = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
+    _webview.UIDelegate = self;
+    _webview.navigationDelegate = self;
+    [self.view addSubview:_webview];
+    
+    NSString *htmlString = [NSString stringWithFormat:@"<html> \n"
+                            "<head> \n"
+                            "<style type=\"text/css\"> \n"
+                            "body {font-size:30px;}\n"
+                            "</style> \n"
+                            "</head> \n"
+                            "<body style=\"word-wrap:break-word; font-family:Arial\">"
+                            "<script type='text/javascript'>"
+                            "window.onload = function(){\n"
+                            "var $img = document.getElementsByTagName('img');\n"
+                            "for(var p in  $img){\n"
+                            "$img[p].style.width = '100%%';\n"
+                            "$img[p].style.height ='auto'\n"
+                            "$img[p].style.align ='center'\n"
+                            "}\n"
+                            "}"
+                            "</script>%@"
+                            "</body>"
+                            "</html>", kHTML];
+    
+    
+    NSData *data = [htmlString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    
+    
+    
+    [self.webview loadData:data MIMEType:@"text/html" characterEncodingName:@"UTF-8" baseURL:[NSURL fileURLWithPath:NSTemporaryDirectory()]];
+
+    char name[] = {'a', 'b', 'c'};
+    unsigned int count = sizeof(name)/sizeof(char);
+    printf("%d", count);
+}
+
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
+{
+    [webView evaluateJavaScript:@"document.documentElement.style.webkitUserSelect='none';" completionHandler:nil];
+    [webView evaluateJavaScript:@"document.activeElement.blur();" completionHandler:nil];
+    // 适当增大字体大小
+    [webView evaluateJavaScript:@"document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust= '105%'" completionHandler:nil];
+}
+
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    NSMutableURLRequest *request = [navigationAction.request mutableCopy];
+    NSLog(@"request————————>%@",request.URL.absoluteString);
+    
+    if ([request.URL.absoluteString containsString:@"blank"]) {
+        decisionHandler(WKNavigationActionPolicyCancel);
+        return;
+    }
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+@end
+
+
